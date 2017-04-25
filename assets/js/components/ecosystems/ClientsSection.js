9537be53f3356f0c17051c8ec3bd27eb46c9d0c8
Travis J. Webb <me@traviswebb.com>
2017-04-25 15:22:39 -0400

    add GE logo to clients section

--- assets/js/components/ecosystems/ClientsSection.js
+++ assets/js/components/ecosystems/ClientsSection.js
@@ -5,17 +5,17 @@ export default class ClientsSection extends Component {
   render () {
     return (
       <div className="client-section">
-        <h2>Our Clients</h2>
+        <h1>Our Clients</h1>
         <div className="client-logos">
-          <img src="//cdn.langa.io/3rdparty/ibmwatson-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/qualcomm-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/homeaway-logo.svg" />
-          <img src="//cdn.langa.io/3rdparty/xogroup-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/mco-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/itprotv-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/milbar-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/oxfordroad-logo.png" />
-          <img src="//cdn.langa.io/3rdparty/underbuilt-logo.png" />
+          <img src="//cdn.langa.io/3rdparty/ibmwatson-logo.png" alt="IBM Watson" />
+          <img src="//cdn.langa.io/3rdparty/qualcomm-logo.png" alt="Qualcomm" />
+          <img src="//cdn.langa.io/3rdparty/ge-logo.jpg" alt="General Electric" />
+          <img src="//cdn.langa.io/3rdparty/homeaway-logo.svg" alt="HomeAway" />
+          <img src="//cdn.langa.io/3rdparty/xogroup-logo.png" alt="XO Group" />
+          <img src="//cdn.langa.io/3rdparty/mco-logo.png" alt="Michigan Colleges Online" />
+          <img src="//cdn.langa.io/3rdparty/itprotv-logo.png" alt="ITPro.TV" />
+          <img src="//cdn.langa.io/3rdparty/oxfordroad-logo.png" alt="Oxford Road" />
+          <img src="//cdn.langa.io/3rdparty/underbuilt-logo.png" alt="Underbuilt" />
         </div>
       </div>
     )
